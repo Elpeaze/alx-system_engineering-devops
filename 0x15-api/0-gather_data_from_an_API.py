aac772978f751e792bd5576cf2fe2d590169f346
Elpeaze <elpeaze@gmail.com>
2023-05-12 10:29:36 +0100

    Gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,18 +1,25 @@
 #!/usr/bin/python3
-"""This script returns information about an emplyees TODO list progress using this REST API."""
+"""Return information about an employee's TODO list progress using this REST API.
+
+This script takes a user ID as a command line argument and queries the JSONPlaceholder
+API to retrieve the user's name and their completed tasks.
+
+"""
+
 import requests
 import sys
+
 
 if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com"
     user_id = int(sys.argv[1])
-    user_endP = "{}/users/{}".format(url, user_id)
-    name = requests.get(user_endP).json().get("name")
-    tasks_endP = "{}/todos".format(url)
-    tasks = requests.get(tasks_endP).json()
+    user_endp = "{}/users/{}".format(url, user_id)
+    name = requests.get(user_endp).json().get("name")
+    tasks_endp = "{}/todos".format(url)
+    tasks = requests.get(tasks_endp).json()
     tasks_user = [task for task in tasks if task.get("userId") == user_id]
     tasks_completed = [task for task in tasks_user if task.get("completed")]
-    print("Employee {} is done with tasks({}/{}):" .format(name, len(tasks_completed), len(tasks_user)))
+    print("Employee {} is done with tasks({}/{}):".format(name, len(tasks_completed), len(tasks_user)))
 
-    for tasks in tasks_completed:
-        print("\t {}".format(tasks.get("title")))
+    for task in tasks_completed:
+        print("\t{}".format(task.get("title")))
