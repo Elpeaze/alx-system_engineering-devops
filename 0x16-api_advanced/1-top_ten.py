0aac87ab35a3c0ff72cc9e3113f0dd21769be1d6
Elpeaze <elpeaze@gmail.com>
2023-05-14 12:44:14 +0100

    Top Ten

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,20 +1,24 @@
-#!/usr/bin/python3
-"""A function that queries the Reddit API
-prints the titles of the first 10 hot posts
-listed for a given subreddit.
-"""
+ 1 #!/usr/bin/python3
+ 2 """
+ 3 A function that queries the Reddit API and prints the titles of the first
+ 4 10 hot posts listed for a given subreddit.
+ 5 """
+ 6 import requests
+ 7
+ 8
+ 9 def top_ten(subreddit):
+10     """
+11     Queries the Reddit API and prints the titles of the first 10 hot posts
+12     listed for the given subreddit.
+13     """
+14     url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+15     headers = {"User-Agent": "MyApp/1.0"}
+16
+17     response = requests.get(url, headers=headers, allow_redirects=False)
+18     if response.status_code == 200:
+19         data = response.json().get('data').get('children')
+20         for entry in data:
+21             print(entry.get('data').get('title'))
+22     else:
+23         print(None)
 
-import requests
-
-
-def top_ten(subreddit):
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
-    headers = {"User-Agent": "MyApp/1.0"}
-
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
-        data = response.json().get('data').get('children')
-        for entry in data:
-            print(entry.get('data').get('title'))
-    else:
-        print(None)
