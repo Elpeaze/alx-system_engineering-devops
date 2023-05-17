1c944b4cbc800cf9c915ca5557f40cad318ea2a9
Elpeaze <elpeaze@gmail.com>
2023-05-17 21:27:58 +0100

    strace is your friend

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,12 +1,42 @@
 # A  file that replaces ".phpp" with ".php"
 
-file { '/var/www/html/wp-settings.php':
-  ensure  => present,
-  content => inline_template('<%=File.read("/var/www/html/wp-settings.php").gsub(/\\.phpp/,".php") %>'),
-  notify  => Service['apache2'],
-}
-
 service { 'apache2':
   ensure => running,
   enable => true,
 }
+
+package { 'apache2':
+  ensure => installed,
+}
+
+service { 'apache2':
+  ensure  => running,
+  enable  => true,
+  require => Package['apache2'],
+}
+
+file { '/var/www/html/index.html':
+  ensure  => present,
+  content => 'This is the correct page.',
+  require => Package['apache2'],
+}
+
+file { '/etc/apache2/sites-available/000-default.conf':
+  ensure  => present,
+  content => template('module/site.conf.erb'),
+  notify  => Service['apache2'],
+  require => Package['apache2'],
+}
+
+file { '/var/www/html/wp-settings.php':
+  ensure  => present,
+  content => inline_template('<%=File.read("/var/www/html/wp-settings.php").gsub(/\\.phpp/,".php") %>'),
+  notify  => Service['apache2'],
+  require => Package['apache2'],
+}
+
+file { '/var/www/html/wp-settings.php':
+  4   ensure  => present,
+  5   content => inline_template('<%=File.read("/var/www/html/wp-settings.php").gsub(/\\.phpp/,".php") %>'),
+  6   notify  => Service['apache2'],
+  7 }
