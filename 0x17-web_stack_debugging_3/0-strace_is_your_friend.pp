d067c42aab32963d6730e5faad092cfbb6511ead
Elpeaze <elpeaze@gmail.com>
2023-05-17 21:07:23 +0100

    strace is your friend

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -3,5 +3,10 @@
 file { '/var/www/html/wp-settings.php':
   ensure  => present,
   content => inline_template('<%=File.read("/var/www/html/wp-settings.php").gsub(/\\.phpp/,".php") %>'),
+  notify  => Service['apache2'],
 }
 
+service { 'apache2':
+  ensure => running,
+  enable => true,
+}
