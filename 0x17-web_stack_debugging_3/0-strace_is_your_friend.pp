c6d608a688afa08c30029c5239d0ff689ab9d768
Elpeaze <elpeaze@gmail.com>
2023-05-17 21:02:44 +0100

    strace is your friend

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,3 +1,5 @@
+# A  file that replaces ".phpp" with ".php"
+
 file { '/var/www/html/wp-settings.php':
   ensure  => present,
   content => inline_template('<%=File.read("/var/www/html/wp-settings.php").gsub(/\\.phpp/,".php") %>'),
