cf1c54a4b5b344d881df4ac29615d922adac905a
Elpeaze <elpeaze@gmail.com>
2023-05-18 21:21:39 +0100

    count it all

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,34 +1,54 @@
 #!/usr/bin/python3
-"""
-This function queries the Reddit API, parses the title of all hot articles,
-and prints a sorted count of given keywords (case-insensitive,
-delimited by spaces. Javascript should count as javascript,
-but java should not).
-"""
+"""queries the Reddit API, parses the title of all hot articles,
+and prints a sorted count of given keywords"""
+
+import re
 import requests
 
 
-def count_words(subreddit, word_list, kwargs={}, after=None):
-    url = "https://www.reddit.com/r/{}/hot/.json?after={}".format(subreddit, after)
-    headers = {"User-Agent": "custom"}
+def count_words(subreddit, word_list, word_dict={},
+                after='', count=0, not_first=0):
+    """queries the Reddit API, parses the title of all hot articles,
+    and prints a sorted count of given keywords"""
 
-    data = requests.get(url, headers=headers, allow_redirects=False)
-    if data.status_code == 200:
-        after = data.json().get('data').get('after')
-        data = data.json().get('data').get('children')
-        for entry in data:
-            parse_list = entry.get('data').get('title').lower().split()
-            for word in parse_list:
-                for element in word_list:
-                    if word == element:
-                        if element in kwargs:
-                            kwargs[element] += 1
-                        else:
-                            kwargs[element] = 1
-        if after:
-            count_words(subreddit, word_list, kwargs, after)
-        else:
-            sorted_dict = {k: v for k, v in sorted(kwargs.items(), key=lambda item: item[1])}
-            [print("{}: {})".format(k, v)) for k, v in sorted_dict.items()]
-    else:
+    headers = {'User-Agent': 'Mozilla/5.0 (Linux; Android 12; SM-S906N\
+               Build/QP1A.190711.020; wv) AppleWebKit/537.36 (KHTML,\
+               like Gecko) Version/4.0 Chrome/80.0.3987.119 Mobile\
+               Safari/537.36'
+               }
+    parameters = {
+        'after': after,
+        'count': count,
+        'limit': 100
+    }
+    try:
+        data = requests.get(
+            'https://www.reddit.com/r/{}/hot/.json'.format(subreddit),
+            headers=headers, params=parameters, allow_redirects=False).json()
+        [filter_word(word_list, word_dict, post['data']['title'])
+         for post in data['data']["children"]]
+
+        if data['data']['after']:
+            count_words(subreddit, word_list, word_dict,
+                        after=data['data']['after'],
+                        count=count+data['data']['dist'], not_first=1)
+            if not_first == 0:
+                for key, value in sorted(word_dict.items(),
+                                         key=lambda val: (-val[1], val[0])):
+                    print('{}:'.format(key), value)
+    except Exception:
         return None
+
+
+def filter_word(word_list, word_dict, string):
+    """filter words count number of words and all it to total in word_dict"""
+    word_list = [word.lower() for word in word_list]
+
+    for word in word_list:
+        reg_string = r'\b{}\b'.format(word)
+        word_count = len(re.findall(reg_string, string, flags=re.IGNORECASE))
+        if word_count > 0:
+            if word_dict.get(word, None) is None:
+                word_dict[word] = word_count
+            else:
+                word_dict[word] = word_dict[word] + word_count
