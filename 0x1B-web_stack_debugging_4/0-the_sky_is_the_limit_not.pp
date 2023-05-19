43c7df4ba1c4a082021ac6393c8dadbd1fb19048
Elpeaze <elpeaze@gmail.com>
2023-05-19 03:43:09 +0100

    Sky is the limit

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,3 +1,11 @@
-file { "/etc/default/nginx":
-  ensure => file,
-  content => "ULIMIT-'-n 5000'\n", }
+# increase number of files nginx can handle simultaneously
+
+exec { 'increase ulimit':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => ['/usr/local/bin/', '/bin/'],
+}
+
+exec { 'restart-nginx':
+  command => 'sudo service nginx restart',
+  path    => ['/usr/local/bin/', '/bin/', '/usr/bin/'],
+}
