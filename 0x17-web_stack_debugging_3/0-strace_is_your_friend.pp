1307d878bf82fe048fd8e3b27af48ad42491f1dd
Elpeaze <111974145+Elpeaze@users.noreply.github.com>
2023-06-19 10:58:53 +0100

    Delete 0x17-web_stack_debugging_3 directory

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++
@@ -1,7 +0,0 @@
-# A  file that replaces ".phpp" with ".php" file extension.
-
-exec { 'Fixed wordpress':
-  command  => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  path     => '/usr/bin:/usr/sbin:/bin',
-  provider => shell,
-}
