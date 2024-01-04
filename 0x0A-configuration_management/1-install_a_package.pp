8c47446dc9d84eb8179aa3e337aae3d20632532f
Elpeaze <elpeaze@gmail.com>
2024-01-04 10:27:15 +0100

    Installs a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,10 @@
 # Using Puppet, install flask from pip3
-package { 'flask':
-  ensure   => '2.1.0',
-  name     => 'flask'
-  provider => 'pip3',
+
+package { 'pip3':
+  ensure   => 'installed',
+ }
+
+exec { 'install-flask':
+  command  => '/usr/bin/pip3 install flask==2.1.0',
+  creates  => '/usr/local/lib/python3.8/dist-packages/flask''
 }
