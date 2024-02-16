33d1463ac170d6ad8981fcfdd10d19e0ed913ff3
Elpeaze <elpeaze@gmail.com>
2024-02-16 01:15:41 +0100

    api querrying

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+i#!/usr/bin/python3
 """A function that queries the reddit API, returns subscribers."""
 
 import requests
@@ -6,7 +6,7 @@ import requests
 
 def number_of_subscribers(subreddit):
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "MyApp/1.0"}
+    headers = {"User-Agent": "Mozilla/5.0"}
 
     data = requests.get(url, headers=header, allow_redirects=False)
 
