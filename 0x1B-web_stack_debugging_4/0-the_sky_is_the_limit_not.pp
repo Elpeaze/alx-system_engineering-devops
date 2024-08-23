fe8afa39d84c7a51689ab349e354d20082b58b0e
Elpeaze <elpeaze@gmail.com>
2024-08-23 11:52:06 +0100

    Modified puppet manifest to meet requirement

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -3,13 +3,5 @@
 file { '/etc/default/nginx':
   ensure  => present,
   content => "ULIMIT='-n 4069'\n",
-  content => template('nginx/nginx.conf.erb'),
   notify  => Service['nginx'],
-}
-# Nginx configuration template (nginx.conf.erb)
-$nginx_conf_content = @("EOF"/L)
-worker_processes auto;
-events {
-    worker_connections 1024;  # Allows each worker to handle 1024 connections
-    multi_accept on;  # Accept multiple connections at once
 }
